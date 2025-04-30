cd1b13fbd266d440fb54db76417f2c0a49f7df75
luisjgro <L24550278@chihuahua2.tecnm.mx>
2025-04-29 19:50:12 -0500

    Add files via upload

---
+++ EVA3_2/src/eva3_2/EVA3_2.java
@@ -0,0 +1,31 @@
+package eva3_2;
+
+public class EVA3_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Inicia Main");
+        A();
+        System.out.println("Termina Main");
+    }
+    
+    public static void A(){
+        System.out.println("Inicia A");
+        B();
+        System.out.println("Termina A");
+    }
+    
+    public static void B(){
+        System.out.println("Inicia B");
+        C();
+        System.out.println("Termina B");
+    }
+    
+    public static void C(){
+        System.out.println("Inicia C");
+        int x = 5, y = 0;
+        int resu = x / y;
+        System.out.println("Resultado = " + resu);
+        System.out.println("Termina C");
+    }
+    
+}
