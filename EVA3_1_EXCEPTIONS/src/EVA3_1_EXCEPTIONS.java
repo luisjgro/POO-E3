8f4dd295a5783fbf251bfb7dedc2289f23e4b26e
luisjgro <L24550278@chihuahua2.tecnm.mx>
2025-04-29 19:53:25 -0500

    Add files via upload

---
+++ EVA3_1_EXCEPTIONS/src/EVA3_1_EXCEPTIONS.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class EVA3_1_EXCEPTIONS {
+
+    public static void main(String[] args) {
+        int x = 5, y = 0;
+        int resu = x / y;
+        
+        //Arithmetic Exceptions
+        System.out.println("Resultado = " + resu);
+        
+        //Indexoutofbounds Exceptions
+        //Arreglos: Leer una posicion que no existe
+        int[] arreglo = new int[5];//0, 1, 2, 3, 4
+        System.out.println(arreglo[10]);
+        
+        //Null Pointer Exception
+        prueba prue = null;
+        prue.toString();
+        
+        //Inputmismatch Exception
+        //Piden un dato de un tipo y les mandan de otro diferente
+        Scanner captu = new Scanner(System.in);
+        int valor = captu.nextInt();
+        System.out.println("Valor = " + valor);
+    }
+    
+}
+
+class prueba{
+    
+}
